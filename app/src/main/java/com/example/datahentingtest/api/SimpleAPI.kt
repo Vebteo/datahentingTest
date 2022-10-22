f0c3eea41eb861ef1ecb56c0b33696c4a0b9b1b2
Vebjørn <vebteo@gmail.com>
2022-10-22 14:31:23 +0200

    forbedringer

--- app/src/main/java/com/example/datahentingtest/api/SimpleAPI.kt
+++ app/src/main/java/com/example/datahentingtest/api/SimpleAPI.kt
@@ -5,5 +5,5 @@ import retrofit2.Response
 import retrofit2.http.GET
 interface SimpleAPI {
     @GET("BrukerQuiz/GameOfThrones")
-    suspend fun getpost(): Response<Post>
+    suspend fun getPost(): Response<Post>
 }
--- app/src/main/java/com/example/datahentingtest/repository/Repository.kt
+++ app/src/main/java/com/example/datahentingtest/repository/Repository.kt
@@ -7,7 +7,7 @@ import retrofit2.Response
 class Repository {
 
     suspend fun getPost(): Response<Post> {
-        return RetrofitInstance.api.getpost()
+        return RetrofitInstance.api.getPost()
     }
 
 }
