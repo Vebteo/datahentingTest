a265ebd78efccf2d48f85e50c87991b2ba181f20
Vebjørn <vebteo@gmail.com>
2022-10-22 13:55:34 +0200

    fikset hamburgermeny

--- app/src/main/java/com/example/datahentingtest/MainActivity.kt
+++ app/src/main/java/com/example/datahentingtest/MainActivity.kt
@@ -4,8 +4,10 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.MenuItem
 import android.view.View
 import android.widget.TextView
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.viewModelFactory
@@ -21,17 +23,30 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var viewModel: MainViewModel
     private lateinit var binding: ActivityMainBinding
+    lateinit var hamburgerIkon: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        setContentView(R.layout.activity_main)
 
+        hamburgerIkon= ActionBarDrawerToggle(this,binding.drawerLayout,R.string.open,R.string.close)
+        binding.drawerLayout.addDrawerListener(hamburgerIkon)
+        hamburgerIkon.syncState()
 
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
         val mainActivity = this
         binding.recyclerView.apply {
             layoutManager = GridLayoutManager(applicationContext,1)
             adapter = KortAdapter(proveListe)
+        }
+
+        binding.navView.setNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.hjemItem->{ velgSide(binding.drawerLayout,1) }
+                R.id.profilItem->{ velgSide(binding.drawerLayout,2) }
+                R.id.loginItem->{ velgSide(binding.drawerLayout,3) }
+            }
+            true
         }
 
         var textView : TextView = findViewById(R.id.tekstTesting)
@@ -53,6 +68,12 @@ class MainActivity : AppCompatActivity() {
             textView.text = response.code().toString()
         }
         })
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(hamburgerIkon.onOptionsItemSelected(item)) {
+            true
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     fun velgSide(view: View, tall: Int) {
